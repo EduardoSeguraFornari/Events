f6452a04521d094750cb293631916659ba5e515d
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2020-01-01 14:51:52 -0300

    first commit

---
+++ Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Events
+//
+//  Created by Eduardo Fornari on 01/01/20.
+//  Copyright © 2020 Eduardo Fornari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
