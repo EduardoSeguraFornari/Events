dd8accd050697cf80e074e95cf598cec0c0feb06
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2020-01-02 01:59:26 -0300

    adding String+isValidEmail

---
+++ Events/Extensions/String+isValidEmail.swift
@@ -0,0 +1,15 @@
+//
+//  String+isValidEmail.swift
+//
+//  Copyright © 2019 Eduardo Fornari. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func isValidEmail() -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return predicate.evaluate(with: self)
+    }
+}
