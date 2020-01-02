fce1e1c3ccb3a4d51ac61a7417e4f107b0a69512
Eduardo Segura Fornari <eduardo.fornari@icloud.com>
2020-01-02 01:24:09 -0300

    setting up keyboard and scroll

--- Events/Features/CheckIn/View/Controller/CheckinViewController.swift
+++ Events/Features/CheckIn/View/Controller/CheckinViewController.swift
@@ -31,6 +31,23 @@ class CheckinViewController: UIViewController {
         return nil
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameTextField.delegate = self
+        emailTextField.delegate = self
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addKeyboardObservers()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeKeyboardObservers()
+    }
+
     // MARK: - Buttons
     @IBAction func confirmButtonDidTap(_ sender: UIButton) {
         print("Do Check-In")
@@ -38,5 +55,70 @@ class CheckinViewController: UIViewController {
 
     @IBAction func cancelButtonDidTap(_ sender: UIButton) {
         dismiss(animated: true)
+    }
+
+    // MARK: - Keyboard Observers
+    private func addKeyboardObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow),
+                                               name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide),
+                                               name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+
+    private func removeKeyboardObservers() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+
+    @objc func keyboardDidShow(_ notification: NSNotification) {
+        let userInfo = notification.userInfo ?? [:]
+        if let keyboardFrame = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+
+            let cancelButtonFrame = cancelButton.frame
+            let cancelButtonBottomPosition = cancelButtonFrame.origin.y + cancelButtonFrame.height
+            let keyboardTopPosition = keyboardFrame.origin.y - keyboardFrame.height
+            let difference = cancelButtonBottomPosition - keyboardTopPosition
+
+            if difference > 0 {
+                let margin: CGFloat = 15
+                scrollView.contentInset.bottom = difference + margin
+                scrollView.scrollIndicatorInsets.bottom = difference + margin
+
+                if nameTextField.isFirstResponder {
+                    scrollView.scrollRectToVisible(nameTextField.frame, animated: true)
+                } else if emailTextField.isFirstResponder {
+                    scrollView.scrollRectToVisible(emailTextField.frame, animated: true)
+                }
+            } else {
+                scrollView.contentInset.bottom = 0
+                scrollView.scrollIndicatorInsets.bottom = 0
+            }
+        }
+    }
+
+    @objc func keyboardDidHide(_ notification: NSNotification) {
+        scrollView.contentInset.bottom = 0
+        scrollView.scrollIndicatorInsets.bottom = 0
+    }
+
+}
+
+// MARK: - UITextFieldDelegate
+extension CheckinViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == nameTextField {
+            emailTextField.becomeFirstResponder()
+        } else if textField == emailTextField {
+            view.endEditing(true)
+        }
+        return true
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == nameTextField {
+            scrollView.scrollRectToVisible(nameTextField.frame, animated: true)
+        } else if textField == emailTextField {
+            scrollView.scrollRectToVisible(emailTextField.frame, animated: true)
+        }
     }
 }
